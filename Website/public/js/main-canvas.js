044ab268669c802bc36c40cc72b1a15bfb24bce1
ThierryBC-24 <77757343+ThierryBC-24@users.noreply.github.com>
2021-11-10 16:58:07 -0500

    Update main-canvas.js

--- Website/public/js/main-canvas.js
+++ Website/public/js/main-canvas.js
@@ -36,7 +36,7 @@ window.addEventListener('load', () => {
             data = context.getImageData(0, 0, imgWidth, imgHeight).data;
             let input = [];
 
-            getBoundingBoxes(img);
+            getBoundingBoxes(img, canvas);
 
             for (let i = 0; i < data.length; i += 4)
                 input.push(data[i + 2] / 255);
@@ -61,7 +61,26 @@ window.addEventListener('load', () => {
     };
 });
 
-function getBoundingBoxes(img) {
-    src = cv.imread(img);
-    console.log("img.size: " + src.size);
+function getBoundingBoxes(img, canvas) {
+    let src = cv.imread(img);
+    cv.cvtColor(src, src, cv.COLOR_RGBA2GRAY, 0);
+    cv.threshold(src, src, 0, 255, cv.THRESH_BINARY);
+    let contours = new cv.MatVector();
+    let hierarchy = new cv.Mat();
+    cv.findContours(src, contours, hierarchy, cv.RETR_CCOMP, cv.CHAIN_APPROX_SIMPLE);
+    let boundingBoxes = [];
+    console.log(contours.size());
+    for (let i = 0; i < contours.size(); i++) {
+        boundingBoxes.push(cv.boundingRect(contours.get(i)));
+    }
+    for (const box of boundingBoxes) {
+        console.log(' width:' + box.width + ' height:' + box.height);
+        let rectangleColor = new cv.Scalar(255, 0, 0);
+        let contoursColor = new cv.Scalar(255, 255, 255);
+        let point1 = new cv.Point(box.x, box.y);
+        let point2 = new cv.Point(box.x + box.width, box.y + box.height);
+        cv.drawContours(src, contours, 0, contoursColor, 1, 8, hierarchy, 100);
+        cv.rectangle(src, point1, point2, rectangleColor, 2, cv.LINE_AA, 0);
+        cv.imshow(canvas, src);
+    }
 }
