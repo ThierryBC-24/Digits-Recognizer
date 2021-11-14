753a41817c393f408c65ab0ae44ab83293c0c3a4
ThierryBC-24 <77757343+ThierryBC-24@users.noreply.github.com>
2021-11-14 14:32:54 -0500

    V1

--- Website/public/js/main-canvas.js
+++ Website/public/js/main-canvas.js
@@ -1,86 +1,116 @@
 window.addEventListener('load', () => {
-    // Objects
-    const canvas = document.querySelector("#sheet");
-    const clearBtn = document.querySelector("#clear-btn");
-    const result = document.querySelector("#result");
-    const context = canvas.getContext("2d");
-    const rect = canvas.getBoundingClientRect();
-    const model = tf.loadLayersModel("../tfjs_model/model.json").then(function(model) {
-        window.model = model;
-    });
-
     // Variables
     let painting = false;
+    let paintingColor = '#3aafa9';
     const imgWidth = 28;
     const imgHeight = 28;
-    const predictionThreshold = 0.7;
+    const padding = 90;
+    let lastDigitX = 0;
+    const predictionThreshold = 0.5;
 
-    canvas.addEventListener('mousedown', () => { painting = true; });
-    canvas.addEventListener('mousemove', (e) => {
+    // Objects
+    const inputCanvas = document.querySelector("#inputSheet");
+    const displayCanvas = document.querySelector("#displaySheet");
+    const inputCTX = inputCanvas.getContext("2d");
+    const displayCTX = displayCanvas.getContext("2d");
+    const clearBtn = document.querySelector("#clear-btn");
+    const result = document.querySelector("#result");
+    const rect = inputCanvas.getBoundingClientRect();
+    tf.loadLayersModel("../tfjs_model/model.json").then(function(model) {
+        window.model = model;
+    });
+    // Hidden canvas for resizing
+    const copyCanvas = document.createElement('canvas');
+    const copyCTX = copyCanvas.getContext("2d");
+    copyCanvas.style.display = 'none';
+    copyCanvas.width = imgWidth;
+    copyCanvas.height = imgHeight;
+    document.body.appendChild(copyCanvas);
+
+    inputCanvas.addEventListener('mousedown', () => { painting = true; });
+    inputCanvas.addEventListener('mousemove', (e) => {
         if (!painting)
             return;
-        context.lineWidth = 30;
-        context.strokeStyle = 'blue';
-        context.lineCap = 'round';
-        context.lineTo(e.clientX - rect.left, e.clientY - rect.top);
-        context.stroke();
-        context.beginPath();
-        context.moveTo(e.clientX - rect.left, e.clientY - rect.top);
+        inputCTX.lineWidth = 15;
+        inputCTX.strokeStyle = paintingColor;
+        inputCTX.lineCap = 'round';
+        inputCTX.lineTo(e.clientX - rect.left, e.clientY - rect.top);
+        inputCTX.stroke();
+        inputCTX.beginPath();
+        inputCTX.moveTo(e.clientX - rect.left, e.clientY - rect.top);
     });
-    canvas.addEventListener('mouseup', () => {
+    inputCanvas.addEventListener('mouseup', () => {
         painting = false;
-        context.beginPath();
+        inputCTX.beginPath();
         let img = new Image();
+
         img.onload = () => {
-            context.drawImage(img, 0, 0, imgWidth, imgHeight);
-            data = context.getImageData(0, 0, imgWidth, imgHeight).data;
+            box = getBoundingBox(img);
             let input = [];
+            // Resize digits to 28 x 28
+            copyCTX.drawImage(
+                inputCanvas, 
+                box.x - padding,
+                box.y - padding,
+                box.width + 2 * padding,
+                box.height + 2 * padding,
+                0,
+                0,
+                imgWidth,
+                imgHeight
+            );
+            data = copyCTX.getImageData(0, 0, imgWidth, imgHeight).data;
+            
+            // Convert rgb from data to inverse black and white
+            for (let i = 0; i < data.length; i += 4) {
+                let rgbValue = data[i] + data[i + 1] + data[i + 2];
+                let color = 0;
+                if (rgbValue > 510) 
+                    color = 255;
+                else if (rgbValue >= 255)
+                    color = 255;
+                input.push(parseFloat(color) / 255);
+            }
 
-            getBoundingBoxes(img, canvas);
-
-            for (let i = 0; i < data.length; i += 4)
-                input.push(data[i + 2] / 255);
-
+            // Update predictions
             window.model.predict([tf.tensor(input).reshape([1, 28, 28, 1])]).array().then(function(scores) {
                 scores = scores[0];
-                console.log("scores: " + scores);
                 let maxScore = Math.max(...scores);
-                if (maxScore > predictionThreshold)
-                    predicted = scores.indexOf(maxScore);
-                else
-                    predicted = "";
-                result.innerHTML += predicted;
+                if (maxScore > predictionThreshold) {
+                    if (box.x > lastDigitX) {
+                        result.innerHTML += scores.indexOf(maxScore);
+                        lastDigitX = box.x;
+                    }
+                    else
+                        result.innerHTML = scores.indexOf(maxScore) + result.innerHTML;
+                        
+                    displayCTX.drawImage(img, 0, 0);
+                    inputCTX.clearRect(0, 0, inputCanvas.width, inputCanvas.height);
+                    copyCTX.clearRect(0, 0, copyCanvas.width, copyCanvas.height);
+                }
+                else {
+                    inputCTX.clearRect(0, 0, inputCanvas.width, inputCanvas.height);
+                    copyCTX.clearRect(0, 0, copyCanvas.width, copyCanvas.height);
+                } 
             });
         }
-        img.src = canvas.toDataURL();
+        img.src = inputCanvas.toDataURL();
     });
 
     clearBtn.onclick = () => {
-        context.clearRect(0, 0, canvas.width, canvas.height);
+        displayCTX.clearRect(0, 0, inputCanvas.width, inputCanvas.height);
         result.innerHTML = "";
+        lastDigitX = 0;
     };
 });
 
-function getBoundingBoxes(img, canvas) {
+function getBoundingBox(img) {
     let src = cv.imread(img);
-    cv.cvtColor(src, src, cv.COLOR_RGBA2GRAY, 0);
-    cv.threshold(src, src, 0, 255, cv.THRESH_BINARY);
     let contours = new cv.MatVector();
     let hierarchy = new cv.Mat();
-    cv.findContours(src, contours, hierarchy, cv.RETR_CCOMP, cv.CHAIN_APPROX_SIMPLE);
-    let boundingBoxes = [];
-    console.log(contours.size());
-    for (let i = 0; i < contours.size(); i++) {
-        boundingBoxes.push(cv.boundingRect(contours.get(i)));
-    }
-    for (const box of boundingBoxes) {
-        console.log(' width:' + box.width + ' height:' + box.height);
-        let rectangleColor = new cv.Scalar(255, 0, 0);
-        let contoursColor = new cv.Scalar(255, 255, 255);
-        let point1 = new cv.Point(box.x, box.y);
-        let point2 = new cv.Point(box.x + box.width, box.y + box.height);
-        cv.drawContours(src, contours, 0, contoursColor, 1, 8, hierarchy, 100);
-        cv.rectangle(src, point1, point2, rectangleColor, 2, cv.LINE_AA, 0);
-        cv.imshow(canvas, src);
-    }
+
+    cv.cvtColor(src, src, cv.COLOR_RGBA2GRAY, 0);
+    cv.threshold(src, src, 0, 255, cv.THRESH_BINARY);
+    cv.findContours(src, contours, hierarchy, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE);
+    return cv.boundingRect(contours.get(0));
 }
--- Website/server.js
+++ Website/server.js
@@ -1,50 +1,30 @@
 const path = require("path");
 const express = require("express");
-const { HTTP_STATUS } = require("./js/utils/http");
-const { FileSystemManager } = require("./js/managers/file_system_manager");
+const { FileSystemManager } = require("./public/js/file_system_manager");
 
 const app = express();
 const PORT = 5000;
-const SIZE_LIMIT = "10mb";
+
 const PUBLIC_PATH = path.join(__dirname + "/public/");
 const fileSystemManager = new FileSystemManager();
 
-/**
- * initialiser les différents middlewares et routes
- */
-
-// afficher chaque nouvelle requête dans la console
 app.use((request, response, next) => {
     console.log(`New HTTP request: ${request.method} ${request.url}`);
     next();
 });
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json({ limit: SIZE_LIMIT }));
-app.use(express.static(PUBLIC_PATH)); // permet de situer les fichiers css, images et scripts qui sont dans /public
+app.use(express.static(PUBLIC_PATH));
 
-/**
- * Middleware qui est exécuté à chaque requête pour retourner le fichier HTML correspondant
- * @todo vérifier dynamiquement si un tel fichier html existe dans le dossier /pages selon le route en question
- * @returns la requête renvoie le fichier correspondant avec un code 200, ou bien une erreur HTTP 404 et la page error.html
- */
 app.get("/*", async(request, response) => {
     let currentRoute = request.path.split("/")[1];
     currentRoute = currentRoute === "" ? "index" : currentRoute;
-    // TODO
     filePath = path.join(PUBLIC_PATH + "pages/" + currentRoute + ".html");
 
     fileSystemManager.checkFile(filePath).then(() => {
-        // Promesse tenue
         response.sendFile(filePath);
     }, () => {
-        // Rejet de la promesse
-        response.status(404).sendFile(PUBLIC_PATH + "pages/erreur.html");
+        response.status(404).sendFile(PUBLIC_PATH + "pages/error.html");
     });
 });
 
-/**
- * Se produit lorsque le serveur commence à écouter sur le port.
- */
 const server = app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
-
 module.exports = server;
